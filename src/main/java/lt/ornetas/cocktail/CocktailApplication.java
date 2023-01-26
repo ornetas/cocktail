0d6e1675be6c226af67e07a1d25d6b563bd573d0
orentas <o.b.ornetas@gmail.com>
2023-01-26 10:32:42 +0200

    Initial commit/dependencies added

---
+++ src/main/java/lt/ornetas/cocktail/CocktailApplication.java
@@ -0,0 +1,13 @@
+package lt.ornetas.cocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailApplication.class, args);
+	}
+
+}
