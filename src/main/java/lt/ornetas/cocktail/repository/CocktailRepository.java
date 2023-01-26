8ba68caab964467a788d88e3e8b9fe33e278649b
orentas <o.b.ornetas@gmail.com>
2023-01-26 13:25:36 +0200

    Repositories created

---
+++ src/main/java/lt/ornetas/cocktail/repository/CocktailRepository.java
@@ -0,0 +1,7 @@
+package lt.ornetas.cocktail.repository;
+
+import lt.ornetas.cocktail.repository.model.Cocktail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CocktailRepository extends CrudRepository<Cocktail, Integer> {
+}
---
+++ src/main/java/lt/ornetas/cocktail/repository/IngredientRepository.java
@@ -0,0 +1,9 @@
+package lt.ornetas.cocktail.repository;
+
+import lt.ornetas.cocktail.repository.model.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, Integer> {
+
+
+}
