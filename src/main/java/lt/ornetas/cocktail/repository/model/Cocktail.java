04a8ba896e6b0d655f31266efa8a41c1dbf99088
orentas <o.b.ornetas@gmail.com>
2023-01-26 13:25:16 +0200

    POJO classes added

---
+++ src/main/java/lt/ornetas/cocktail/repository/model/Cocktail.java
@@ -0,0 +1,27 @@
+package lt.ornetas.cocktail.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "city")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+public class Cocktail {
+
+    @Id
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+
+}
