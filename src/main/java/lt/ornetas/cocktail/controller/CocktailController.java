a1f76e8e6c2c16cbc030bc55ec09b40e1443970a
orentas <o.b.ornetas@gmail.com>
2023-01-26 14:35:01 +0200

    Mapping done to get all and to get by name like

--- src/main/java/lt/ornetas/cocktail/controller/CocktailController.java
+++ src/main/java/lt/ornetas/cocktail/controller/CocktailController.java
@@ -1,4 +1,33 @@
 package lt.ornetas.cocktail.controller;
 
+import lt.ornetas.cocktail.repository.model.Cocktail;
+import lt.ornetas.cocktail.service.CocktailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/cocktailmapping") //localhost:8080/cocktailmapping
+
 public class CocktailController {
+
+    @Autowired
+    private CocktailService cocktailService;
+
+    // http://localhost:8080/cocktailmapping/cocktail/all
+    @GetMapping(path = "/cocktail/all")
+    public @ResponseBody List<Cocktail> getAllCocktails() {
+        return cocktailService.getAllCocktails();
+    }
+
+    // http://localhost:8080/cocktailmapping/cocktail/like/lime
+    @GetMapping(path = "/cocktail/like/{name}")
+    public @ResponseBody List<Cocktail> getCocktailByNameLike(@PathVariable String name) {
+        return cocktailService.getCocktailByNameLike("%" + name + "%");
+    }
 }
--- src/main/java/lt/ornetas/cocktail/controller/IngredientController.java
+++ src/main/java/lt/ornetas/cocktail/controller/IngredientController.java
@@ -1,4 +1,40 @@
 package lt.ornetas.cocktail.controller;
 
+import lt.ornetas.cocktail.repository.model.Ingredient;
+import lt.ornetas.cocktail.service.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/ingredientmapping") //localhost:8080/ingredientmapping
+
 public class IngredientController {
+
+    @Autowired
+    private IngredientService ingredientService;
+
+    // http://localhost:8080/ingredientmapping/test
+    @GetMapping(path = "/test")
+    public @ResponseBody String getTestPage() {
+        return "This is some test from SPRING!";
+    }
+
+    // http://localhost:8080/ingredientmapping/ingredient/all
+    @GetMapping(path = "/ingredient/all")
+    public @ResponseBody List<Ingredient> getAllIngredients() {
+        return ingredientService.getAllIngredients();
+    }
+
+    // http://localhost:8080/ingredientmapping/ingredient/like/cuba
+    @GetMapping(path = "/ingredient/like/{name}")
+    public @ResponseBody List<Ingredient> getIngredientByNameLike(@PathVariable String name) {
+        return ingredientService.getIngredientByNameLike("%" + name + "%");
+    }
+
 }
