639b4b8fd00ad0818b67b304a8802cfbe68073a2
orentas <o.b.ornetas@gmail.com>
2023-01-26 13:43:07 +0200

    Getters created in service classes

--- src/main/java/lt/ornetas/cocktail/service/CocktailService.java
+++ src/main/java/lt/ornetas/cocktail/service/CocktailService.java
@@ -1,4 +1,25 @@
 package lt.ornetas.cocktail.service;
 
+import lt.ornetas.cocktail.repository.CocktailRepository;
+import lt.ornetas.cocktail.repository.model.Cocktail;
+import lt.ornetas.cocktail.repository.model.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CocktailService {
+
+    @Autowired
+    private CocktailRepository cocktailRepository;
+
+    public List<Cocktail> getAllCocktails(){
+        return (List<Cocktail>) cocktailRepository.findAll();
+    }
+
+    public List<Cocktail> getCocktailByNameLike(String cocktailName){
+        return (List<Cocktail>) cocktailRepository.findByNameLike(cocktailName);
+    }
+
 }
--- src/main/java/lt/ornetas/cocktail/service/IngredientService.java
+++ src/main/java/lt/ornetas/cocktail/service/IngredientService.java
@@ -1,4 +1,25 @@
 package lt.ornetas.cocktail.service;
 
+import lt.ornetas.cocktail.repository.CocktailRepository;
+import lt.ornetas.cocktail.repository.IngredientRepository;
+import lt.ornetas.cocktail.repository.model.Cocktail;
+import lt.ornetas.cocktail.repository.model.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class IngredientService {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    public List<Ingredient> getAllIngredients(){
+        return (List<Ingredient>) ingredientRepository.findAll();
+    }
+
+    public List<Ingredient> getIngredientByNameLike(String ingredientName){
+        return (List<Ingredient>) ingredientRepository.findByNameLike(ingredientName);
+    }
 }
